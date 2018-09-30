1f54343cbcf943157536d6a1e8879ef211d004af
Jose Villarroel <oscarclarke9900@gmail.com>
2018-09-30 19:39:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Project Title
+
+Small project practicing basics of web developing.
+
+## Getting Started
+
+Most necessary modules are already on the repository, additional steps will be specified below.
+
+### Prerequisites
+
+* Mongo DB installed and running.
+* Node.js
+
+
+### Installing
+
+On the main folder run. (if MongoDB daemon is not running a listening on port specified on app.js file it will not run successfully)
+
+```
+node app.js
+```
+
+The response should be
+
+```
+Venezuela Tour has started
+```
+
+Check node version to be sure node is installed with
+
+```
+node -v
+```
+
+## Running the tests
+
+In order to add new sites go to /toursites/new
+
+## Built With
+
+* [Express](http://expressjs.com/) - The web framework used
+* [Node.js](https://nodejs.org/) - Framework used for server side JavaScript programming
+* [MongoDB](https://rometools.github.io/rome/) - Database used
+
+
+## Authors
+
+* **Colt Steele** - *Initial work* - [Colt](https://github.com/Colt)
+* **Jose Villarroel** - *Adaptation to Venezuela Toursite idea* - [Wayhem](https://github.com/Wayhem)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.txt](LICENSE.txt) file for details
---
+++ app.js
@@ -0,0 +1,80 @@
+// insert modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/venezuela_tours', { useNewUrlParser: true });
+var app = express();
+
+var toursiteSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Toursite = mongoose.model("Toursite", toursiteSchema);
+
+// Toursite.create({
+//   name:"Roraima",
+//   image:"http://miriadna.com/desctopwalls/images/max/Mount-Roraima.jpg",
+//   description: "En el sector Oriental del parque nacional Canaima, en medio de la región conocida como la \"Gran Sabana\", se encuentra el tepuy más alto de todos: El Roraima."
+// }, function(err, toursite) {
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log("New Toursite!");
+//     console.log(toursite);
+//   }
+// });
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
+app.get("/toursites", function(req, res) {
+  Toursite.find({}, function (err, toursites) {
+    if(err){
+      console.log(err);
+    } else {
+      res.render("index", {sites: toursites});
+    }
+  });
+});
+
+app.post("/toursites", function(req, res) {
+  var name = req.body.name;
+  var image = req.body.image;
+  var desc = req.body.description;
+  var newToursite = {name: name, image: image, description: desc};
+
+  Toursite.create(newToursite, function (err, newCreat){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect("/toursites");
+    }
+  });
+});
+
+app.get("/toursites/new", function(req, res) {
+  res.render("new");
+});
+
+app.get("/toursites/:id", function(req, res){
+  Toursite.findById(req.params.id, function (err, foundSite){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("show", { toursite: foundSite });
+    }
+  });
+});
+
+app.get("/", function(req, res){
+  res.render('landing');
+});
+
+app.listen(3000, function(){
+  console.log("Venezuela Tour has started");
+});
